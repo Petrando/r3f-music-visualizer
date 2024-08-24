6d8adab129a7958cda76b78db947700209a1d633
Petrando <petrandorichard@gmail.com>
2024-08-24 13:23:50 +0800

    fix: delete unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useRef, Suspense } from 'react'
-import { Canvas, useFrame, extend } from '@react-three/fiber'
-import { Icosahedron, useHelper, OrbitControls } from '@react-three/drei'
-import * as THREE from 'three'
-import { EffectComposer } from '@react-three/postprocessing'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 import { Scene } from './components/Scene'
 
 const App = () => (
-  <div style={{width: "100vw", height: "100vh"}}>
+  <div style={{width: "100vw", height: "100vh", position: "relative"}}>
+    
        <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
         <color attach="background" args={["#111827"]} />
         <ambientLight intensity={0.5} />
@@ -15,15 +14,8 @@ const App = () => (
         <OrbitControls />
         <Suspense fallback={null}>
           <ambientLight />
-          <directionalLight />
-          {/*<mesh scale={25} position={-1}>
-            <planeGeometry />
-            <meshStandardMaterial color="#ffffff" />
-          </mesh>*/}
+          <directionalLight />          
           <Scene />
-          {/*<EffectComposer>
-            <Vignette eskil={false} offset={0.3} darkness={0.9} />
-          </EffectComposer>*/}
         </Suspense>
       </Canvas>
   </div>
