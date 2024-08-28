87c834d7dbdb70ec02b6a0d757c9e4a5a79f6f4c
Petrando <petrandorichard@gmail.com>
2024-08-28 16:16:23 +0800

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Your App Name',
+        short_name: 'AppName',
+        description: 'Your app description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/music-vis-icon.png',
+            sizes: '498x501',
+            type: 'image/png',
+          },          
+        ],
+      },
+    }),
+  ],
 })
